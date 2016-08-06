0d44fd9be951e389eb8998a621fbfffa91b6392e
Andi-Lo <contact@andreaslorer.de>
2016-08-06 14:58:43 +0200

    removed unused configs

--- site/config/datepicker_example_config.php
+++ site/config/datepicker_example_config.php
@@ -29,8 +29,8 @@ c::set('dp_dateFormat', 'd-m-Y');
  * Feel free to delete unused 
  * For better understanding see: https://chmln.github.io/flatpickr/#options
 */
-c::set('dp_enableTime', 'true');
-c::set('dp_noCalendar', 'true');
+c::set('dp_enableTime', true);
+c::set('dp_noCalendar', true);
 c::set('dp_enableTime', false);
 c::set('dp_altFormat', 'F j, Y');
 c::set('dp_altInput', false);
@@ -43,20 +43,10 @@ c::set('dp_enableSeconds', false);
 c::set('dp_noCalendar', false);
 c::set('dp_hourIncrement', 1);
 c::set('dp_minuteIncrement',  5);
-c::set('dp_inline', false);
-c::set('dp_static',  false);
-c::set('dp_wrap', false);
 c::set('dp_maxDate',  null);
 c::set('dp_minDate', null);
-c::set('dp_onChange',  null);
-c::set('dp_onOpen', null);
-c::set('dp_onClose',  null);
-c::set('dp_parseDate', false);
 c::set('dp_shorthandCurrentMonth', false);
 c::set('dp_weekNumbers', false);
 c::set('dp_time_24hr',  false);
-c::set('dp_utc', false);
-c::set('dp_prevArrow',  '&lt;');
-c::set('dp_nextArrow', '&at;');
 
 
