e1f2c9b91a50881c8c9515cd9ab4061977e028af
Andi-Lo <contact@andreaslorer.de>
2016-08-06 15:19:48 +0200

    removed duplicated config entry

--- site/config/datepicker_example_config.php
+++ site/config/datepicker_example_config.php
@@ -14,7 +14,7 @@ DOCUMENTATION: https://github.com/Andi-Lo/kirby-datepicker#options
 /* 
  * European Format: 'd-m-Y'
  * American Format: 'm/d/Y'
- * Fancy Format: 		'l,F j,Y' e.g.: Friday, August 12, 2016
+ * Fancy Format: 		'l, F j,Y' e.g.: Friday, August 12, 2016
  * See list of all available date formats: https://github.com/Andi-Lo/kirby-datepicker#date-format-characters
 */
 c::set('dp_dateFormat', 'd-m-Y');
@@ -31,18 +31,15 @@ c::set("panel.stylesheet", "assets/css/datepicker/yourThemeFile.css");
  * Feel free to delete unused 
  * Full documentation of options: https://github.com/Andi-Lo/kirby-datepicker#options
 */
-c::set('dp_enableTime', true);
-c::set('dp_noCalendar', true);
 c::set('dp_enableTime', false);
+c::set('dp_noCalendar', false);
 c::set('dp_altFormat', 'F j, Y');
 c::set('dp_altInput', false);
 c::set('dp_altInputClass', '');
 c::set('dp_allowInput', false);
 c::set('dp_clickOpens',  true);
 c::set('dp_defaultDate', '');
-c::set('dp_enableTime', false);
 c::set('dp_enableSeconds', false);
-c::set('dp_noCalendar', false);
 c::set('dp_hourIncrement', 1);
 c::set('dp_minuteIncrement',  5);
 c::set('dp_maxDate',  null);
